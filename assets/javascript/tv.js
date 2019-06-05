8ffdd021334fa3c46e25663053621c0788aa212b
Phillip Chang <phillip-chang@live.com>
2019-06-04 22:58:07 -0700

    Add div tags to append gifs to tv-screen

--- assets/javascript/tv.js
+++ assets/javascript/tv.js
@@ -2,19 +2,35 @@
 var tvShows = ["Family Guy", "American Dad", "Spongebob", "One Punch Man"];
 
 // Function to display Tv Show Gifs
+
 function displayTvShow(){
-    $("#tv-screen").empty();
+    $(".tv-screen").empty();
     var tvshow = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     tvshow + "&api_key=LJgKtX1udypIR6BSXOCsDIUP4gt2dGG0";
+    console.log(queryURL);
 
     $.ajax({
-        url:queryURL,
+        url: queryURL,
         method: "GET"
     }).then(function(response){
-        console.log(response);    
-    $(".tv-screen").append()
-    });
+        console.log("this is the response:" +response);
+        var results = response.data;
+        console.log("this is response data" +response.data);
+
+    for ( i = 0; i < 10; i++){
+        var showDiv = $("<div>");
+        var showImage = $("<img>");
+        showImage.attr('src', results[i].images.fixed_height.url);
+        showImage.attr('data-still', results[i].images.fixed_height_still.url);
+        showImage.attr('data-animate', results[i].images.fixed_height.url);
+        showImage.attr('data-state', "still");
+        showImage.addClass('tv-gif');
+        showDiv.append(showImage);
+        console.log("this is showdiv: " +showDiv);
+    $(".tv-screen").append(showDiv);
+    }    
+});
 }
 
 // Function to create new buttons
@@ -23,8 +39,8 @@ function newRemotes(){
     $(".remote-controls").empty();
     for ( i = 0; i < tvShows.length; i++){
         var remote = $("<button>");
-        remote.addClass("tvshow");
-        remote.attr("data-name", tvShows[i]);
+        remote.addClass('tvshow');
+        remote.attr('data-name', tvShows[i]);
         remote.text(tvShows[i]);
         $(".remote-controls").append(remote);
     }
@@ -35,10 +51,11 @@ $("#add-tvshow").on("click", function(event){
 event.preventDefault();
 var tvshow = $("#tvshow-input").val().trim();
 tvShows.push(tvshow);
+console.log("this is tvshow input:" +tvshow);  
 newRemotes();
 });
 
-$(document).on("click", ".movie", displayTvShow);
+$(document).on("click", ".tvshow", displayTvShow);
 
 newRemotes();
 
